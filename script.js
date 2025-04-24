112f0078012b608e56ca7a2f70345e4fecef11d4
sakthi-1706 <sakthimurugesanmu@gmail.com>
2025-04-24 12:41:07 +0530

    Access Key Fixed

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var imgbox=document.getElementById("imgbox")
 var qrimg=document.getElementById("qrimg")
 var qrtxt=document.getElementById("qrtxt")
+var btn=document.getElementById("btn")
 
-function qrgenerator(){
     if(qrtxt.value.length>0)
     {
-    qrimg.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrtxt.value
-    imgbox.classList.add("show-img")
+       qrtxt.addEventListener("keydown",function(event){
+        if(event.key ==="Enter")
+        {
+            btn.click()
+            event.preventDefault()
+            qrimg.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrtxt.value
+            imgbox.classList.add("show-img")
+        }
+       }) 
+    
     }
     else{
         qrtxt.classList.add("error")
@@ -14,4 +22,3 @@ function qrgenerator(){
             qrtxt.classList.remove("error")
         },1000)
     }
-}
